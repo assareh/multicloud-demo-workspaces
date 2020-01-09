20866ca9cc7107b597924aab49023ee4b4c17a98
assareh <assareh@hashicorp.com>
2020-01-09 12:44:37 -0800

    updated teams and access

--- teams.tf
+++ teams.tf
@@ -14,21 +14,35 @@ resource "tfe_team" "manager_team" {
 }
 
 resource "tfe_team" "qa_team" {
-  name         = "qa-team"
+  name         = "qa"
   organization = var.organization
 }
 
-resource "tfe_team_access" "dev_access" {
+resource "tfe_team_access" "dev_ws_dev_team" {
   access       = "write"
   team_id      = tfe_team.dev_team.id
   workspace_id = module.aws-dev-workspace.id
 }
 
-resource "tfe_team_access" "qa_access" {
+resource "tfe_team_access" "dev_ws_qa_team" {
   access       = "read"
+  team_id      = tfe_team.qa_team.id
+  workspace_id = module.aws-dev-workspace.id
+}
+
+resource "tfe_team_access" "qa_ws_qa_team" {
+  access       = "write"
   team_id      = tfe_team.qa_team.id
   workspace_id = module.aws-qa-workspace.id
 }
 
-# give people team memberships
+resource "tfe_team_access" "qa_ws_dev_team" {
+  access       = "read"
+  team_id      = tfe_team.dev_team.id
+  workspace_id = module.aws-qa-workspace.id
+}
 
+resource "tfe_team_member" "iamdevloper" {
+  team_id  = tfe_team.dev_team.id
+  username = "iamdevloper"
+}
