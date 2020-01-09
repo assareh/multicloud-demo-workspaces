9b9aa9b12b1f99c4e77e9ee611937e5b33f9b8c7
assareh <assareh@hashicorp.com>
2020-01-09 10:53:38 -0800

    added new teams

--- teams.tf
+++ teams.tf
@@ -1,20 +1,30 @@
-resource "tfe_team" "devs" {
+resource "tfe_team" "admin_team" {
+  name         = "admins"
+  organization = var.organization
+}
+
+resource "tfe_team" "dev_team" {
   name         = "developers"
   organization = var.organization
 }
 
-resource "tfe_team" "qa" {
+resource "tfe_team" "manager_team" {
+  name         = "managers"
+  organization = var.organization
+}
+
+resource "tfe_team" "qa_team" {
   name         = "qa-team"
   organization = var.organization
 }
 
-resource "tfe_team_access" "dev" {
+resource "tfe_team_access" "dev_access" {
   access       = "write"
   team_id      = tfe_team.devs.id
   workspace_id = module.aws-dev-workspace.id
 }
 
-resource "tfe_team_access" "qa" {
+resource "tfe_team_access" "qa_access" {
   access       = "read"
   team_id      = tfe_team.qa.id
   workspace_id = module.aws-qa-workspace.id
