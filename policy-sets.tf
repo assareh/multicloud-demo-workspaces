f25b2a04c37ef92284717eff666f259e5119f7d4
assareh <assareh@hashicorp.com>
2020-01-13 16:38:13 -0800

    added first gen aws sentinel policy

--- policy-sets.tf
+++ policy-sets.tf
@@ -1,8 +1,21 @@
-resource "tfe_policy_set" "terraform_guides_aws_policy_set" {
-  name                   = "Terraform-Guides-AWS"
-  description            = "A collection of example Sentinel policies for AWS."
+resource "tfe_policy_set" "terraform_guides_aws_v2_policy_set" {
+  name                   = "Terraform-Guides-AWS-Second-Gen"
+  description            = "A collection of example Sentinel policies for AWS (Second Generation)."
   organization           = var.organization
   policies_path          = "/governance/second-generation/aws"
+  workspace_external_ids = [module.aws_dev_workspace.external_id, module.aws_qa_workspace.external_id, module.aws_prod_workspace.external_id]
+
+  vcs_repo {
+    identifier     = "assareh/terraform-guides"
+    oauth_token_id = var.vcs_oauth_token_id
+  }
+}
+
+resource "tfe_policy_set" "terraform_guides_aws_v1_policy_set" {
+  name                   = "Terraform-Guides-AWS-First-Gen"
+  description            = "A collection of example Sentinel policies for AWS (First Generation)."
+  organization           = var.organization
+  policies_path          = "/governance/first-generation/aws"
   workspace_external_ids = [module.aws_dev_workspace.external_id, module.aws_qa_workspace.external_id, module.aws_prod_workspace.external_id]
 
   vcs_repo {
