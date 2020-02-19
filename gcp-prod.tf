cfe3c7bbae27089f6bcc6d86c8c69bdcc37bf643
assareh <assareh@hashicorp.com>
2020-02-19 15:55:35 -0800

    bug fix

--- gcp-prod.tf
+++ gcp-prod.tf
@@ -31,7 +31,7 @@ resource "tfe_notification_configuration" "gcp_notification" {
 }
 
 # GCP credentials
-resource "tfe_variable" "gcp_credentials_variable" {
+resource "tfe_variable" "prod_gcp_credentials_variable" {
   key          = "gcp_credentials"
   value        = data.local_file.gcp_credentials.content
   category     = "terraform"
