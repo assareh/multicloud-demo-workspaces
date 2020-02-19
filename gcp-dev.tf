33e22136bf7b3ea1fe8b0a31f64e6000fe3ad1e1
assareh <assareh@hashicorp.com>
2020-02-19 15:54:58 -0800

    three gcp branches

--- gcp-dev.tf
+++ gcp-dev.tf
@@ -1,10 +1,10 @@
-module "gcp_master_workspace" {
+module "gcp_dev_workspace" {
   source       = "./workspace"
   organization = var.organization
   token        = var.token
 
   vcs_repo           = "assareh/gcp-compute-instance"
-  vcs_branch         = "master"
+  vcs_branch         = "dev"
   vcs_oauth_token_id = var.vcs_oauth_token_id
 
   terraform_version = "0.12.19"
@@ -27,7 +27,7 @@ resource "tfe_notification_configuration" "gcp_notification" {
   destination_type      = "slack"
   triggers              = ["run:needs_attention", "run:errored"]
   url                   = var.webhook_url
-  workspace_external_id = module.gcp_master_workspace.external_id
+  workspace_external_id = module.gcp_dev_workspace.external_id
 }
 
 # GCP credentials
@@ -36,5 +36,5 @@ resource "tfe_variable" "gcp_credentials_variable" {
   value        = data.local_file.gcp_credentials.content
   category     = "terraform"
   sensitive    = true
-  workspace_id = module.gcp_master_workspace.id
+  workspace_id = module.gcp_dev_workspace.id
 }
---
+++ gcp-stage.tf
@@ -0,0 +1,40 @@
+module "gcp_stage_workspace" {
+  source       = "./workspace"
+  organization = var.organization
+  token        = var.token
+
+  vcs_repo           = "assareh/gcp-compute-instance"
+  vcs_branch         = "stage"
+  vcs_oauth_token_id = var.vcs_oauth_token_id
+
+  terraform_version = "0.12.19"
+  auto_apply        = "false"
+  allow_destroy     = "0"
+
+  workspace_variables = [
+    {
+      key       = "gcp_project"
+      value     = "andy-assareh-demo"
+      category  = "terraform"
+      sensitive = false
+    }
+  ]
+}
+
+resource "tfe_notification_configuration" "gcp_notification" {
+  name                  = "Slack notifications to #test-assareh-alerts"
+  enabled               = true
+  destination_type      = "slack"
+  triggers              = ["run:needs_attention", "run:errored"]
+  url                   = var.webhook_url
+  workspace_external_id = module.gcp_stage_workspace.external_id
+}
+
+# GCP credentials
+resource "tfe_variable" "gcp_credentials_variable" {
+  key          = "gcp_credentials"
+  value        = data.local_file.gcp_credentials.content
+  category     = "terraform"
+  sensitive    = true
+  workspace_id = module.gcp_stage_workspace.id
+}
