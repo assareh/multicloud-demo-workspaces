fb73e3bdc150dc017f78abea6a61d9f82936b242
assareh <assareh@hashicorp.com>
2020-02-19 21:00:01 -0800

    added gke cluster workspaces

--- policy-sets.tf
+++ policy-sets.tf
@@ -42,7 +42,7 @@ resource "tfe_policy_set" "terraform_guides_gcp_v2_policy_set" {
   description            = "A collection of example Sentinel policies for GCP (Second Generation)."
   organization           = var.organization
   policies_path          = "/governance/second-generation/gcp"
-  workspace_external_ids = [module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id]
+  workspace_external_ids = [module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id, module.gcp_gke_dev_workspace.external_id, module.gcp_gke_stage_workspace.external_id, module.gcp_gke_prod_workspace.external_id]
 
   vcs_repo {
     identifier     = "assareh/terraform-guides"
@@ -55,7 +55,7 @@ resource "tfe_policy_set" "terraform_guides_gcp_v1_policy_set_" {
   description            = "A collection of example Sentinel policies for GCP (First Generation)."
   organization           = var.organization
   policies_path          = "/governance/first-generation/gcp"
-  workspace_external_ids = [module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id]
+  workspace_external_ids = [module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id, module.gcp_gke_dev_workspace.external_id, module.gcp_gke_stage_workspace.external_id, module.gcp_gke_prod_workspace.external_id]
 
   vcs_repo {
     identifier     = "assareh/terraform-guides"
@@ -69,7 +69,7 @@ resource "tfe_policy_set" "terraform_guides_agnostic_policy_set" {
   description            = "A collection of example cloud agnostic Sentinel policies."
   organization           = var.organization
   policies_path          = "/governance/second-generation/cloud-agnostic"
-  workspace_external_ids = [module.aws_dev_workspace.external_id, module.aws_qa_workspace.external_id, module.aws_prod_workspace.external_id, module.azure_master_workspace.external_id, module.azure_dev_workspace.external_id, module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id]
+  workspace_external_ids = [module.aws_dev_workspace.external_id, module.aws_qa_workspace.external_id, module.aws_prod_workspace.external_id, module.azure_master_workspace.external_id, module.azure_dev_workspace.external_id, module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id, module.gcp_gke_dev_workspace.external_id, module.gcp_gke_stage_workspace.external_id, module.gcp_gke_prod_workspace.external_id]
 
   vcs_repo {
     identifier     = "assareh/terraform-guides"
