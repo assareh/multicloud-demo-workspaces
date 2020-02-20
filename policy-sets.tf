d5c996394f6136858811c796ccec400adb91646c
assareh <assareh@hashicorp.com>
2020-02-19 16:17:55 -0800

    enhanced gcp policy sets and teams

--- policy-sets.tf
+++ policy-sets.tf
@@ -37,12 +37,12 @@ resource "tfe_policy_set" "terraform_guides_azure_policy_set" {
   }
 }
 
-resource "tfe_policy_set" "terraform_guides_gcp_policy_set" {
-  name                   = "Terraform-Guides-GCP"
-  description            = "A collection of example Sentinel policies for GCP."
+resource "tfe_policy_set" "terraform_guides_gcp_v2_policy_set" {
+  name                   = "Terraform-Guides-GCP-Second-Gen"
+  description            = "A collection of example Sentinel policies for GCP (Second Generation)."
   organization           = var.organization
   policies_path          = "/governance/second-generation/gcp"
-  workspace_external_ids = [module.gcp_master_workspace.external_id]
+  workspace_external_ids = [module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id]
 
   vcs_repo {
     identifier     = "assareh/terraform-guides"
@@ -50,12 +50,26 @@ resource "tfe_policy_set" "terraform_guides_gcp_policy_set" {
   }
 }
 
+resource "tfe_policy_set" "terraform_guides_gcp_v1_policy_set_" {
+  name                   = "Terraform-Guides-GCP-First-Gen"
+  description            = "A collection of example Sentinel policies for GCP (First Generation)."
+  organization           = var.organization
+  policies_path          = "/governance/first-generation/gcp"
+  workspace_external_ids = [module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id]
+
+  vcs_repo {
+    identifier     = "assareh/terraform-guides"
+    oauth_token_id = var.vcs_oauth_token_id
+  }
+}
+
+# need a better way to represent these lists
 resource "tfe_policy_set" "terraform_guides_agnostic_policy_set" {
   name                   = "Terraform-Guides-Agnostic"
   description            = "A collection of example cloud agnostic Sentinel policies."
   organization           = var.organization
   policies_path          = "/governance/second-generation/cloud-agnostic"
-  workspace_external_ids = [module.aws_dev_workspace.external_id, module.aws_qa_workspace.external_id, module.aws_prod_workspace.external_id, module.azure_master_workspace.external_id, module.azure_dev_workspace.external_id, module.gcp_master_workspace.external_id]
+  workspace_external_ids = [module.aws_dev_workspace.external_id, module.aws_qa_workspace.external_id, module.aws_prod_workspace.external_id, module.azure_master_workspace.external_id, module.azure_dev_workspace.external_id, module.gcp_dev_workspace.external_id, module.gcp_stage_workspace.external_id, module.gcp_prod_workspace.external_id]
 
   vcs_repo {
     identifier     = "assareh/terraform-guides"
--- teams.tf
+++ teams.tf
@@ -36,6 +36,24 @@ resource "tfe_team_access" "qa_ws_qa_team" {
   workspace_id = module.aws_qa_workspace.id
 }
 
+resource "tfe_team_access" "gcp_dev_ws_dev_team" {
+  access       = "plan"
+  team_id      = tfe_team.dev_team.id
+  workspace_id = module.gcp_dev_workspace.id
+}
+
+resource "tfe_team_access" "gcp_dev_ws_qa_team" {
+  access       = "read"
+  team_id      = tfe_team.qa_team.id
+  workspace_id = module.gcp_dev_workspace.id
+}
+
+resource "tfe_team_access" "gcp_stage_ws_qa_team" {
+  access       = "write"
+  team_id      = tfe_team.qa_team.id
+  workspace_id = module.gcp_stage_workspace.id
+}
+
 resource "tfe_team_access" "azure_dev_ws_dev_team" {
   access       = "plan"
   team_id      = tfe_team.dev_team.id
